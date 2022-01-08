719d8031a8d36e6623e2d1c146c19216d9a33525
Pranjal Shivhare <pranjalshivhare06@gmail.com>
2022-01-08 11:06:35 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const myfirstelement = <h1>hello react</h1>
+
+// ReactDOM.render(myfirstelement,document.getElementById('root'));
+
+// //class
+
+// class Car {
+//   constructor(name){
+//     this.brand = name;
+//   }
+
+//   present() {
+//     return 'i have a ' + this.brand;
+//   }
+// }
+
+// class Model extends Car {
+//   constructor(name,mod) {
+//     super(name);
+//     this.model = mod;
+//   }
+
+//   show(){
+//     return this.present() + 'it is a' + this.model;
+//   }
+// }
+
+// const mycar = new Car("ford");
+// mycar.present();
+
+// //arrow function
+// //before
+// hello = function(){
+//   return "hello world";
+// }
+
+// hello = () => {
+//   return "hello world";
+// }
+
+// //works only if function have only one statment
+// hello = () => "hello world";
+
+// //with parameters
+// hello = (val) => "hello" + Val;
+
+// //array
+ 
+// const myArray = ['a','b','c'];
+// const myList = myArray.map((item) => <p>{item}</p>)
+
+// const vehicles = ['m','f','e'];
+// const car = vehicles[0];
+// const [car,truck,suv] = vehicles;
+
+
+// function  calculate(a,b){
+//   const add = a+b;
+//   const sub = a-b;
+
+//   return [add,sub];
+// }
+
+// const [add,sub] = calculate(4,5);
+
+// //class component
+// class Car extends React.Component {
+//   render() {
+//     return <h2>hii</h2>;
+//   }
+// }
+
+// //function component
+// function Car() {
+//   return <h2>hii</h2>;
+// }
+
+
+// //rendering a component
+// reactDom.render(<Car/>,document.getElementById('root'));
+
+
+// function car() {
+//   return <h2>pranjal</h2>;
+// }
+
+// function Garage() {
+//   return (
+//     <>
+//       <h1>who</h1>
+//       <car/>
+//     </>
+//   )
+// }
